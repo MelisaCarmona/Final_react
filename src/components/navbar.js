c6e2f881360c89070801a1a192b52620aa7e4e60
melicaso1624 <melicarmona35@gmail.com>
2021-08-04 00:04:28 -0500

    Cuadrando estructura

---
+++ src/components/navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './css/navbar.css';
+
+
+export const Navbar = () => {
+    return (
+    <header>
+        <h1>Fragance Style</h1>
+        <nav id="menu_principal">
+            <ul>
+                <li>Catálogo</li>
+                <li>Contacto</li>
+                <li><i type="button"  style={{ fontWeight: 570 }} className="fas fa-shopping-cart" id="carrito"></i></li>
+                <li><i className="fas fa-user" style={{ fontWeight: 70 }} type="button" data-bs-toggle="modal" data-bs-target="#registerUser"></i></li>
+                <li> <button type="button" id="cierreSesion" className="btn btn-outline-light">Cerrar Sesión</button></li>
+            </ul>
+        </nav>
+   </header>
+   );
+}
