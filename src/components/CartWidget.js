db229b1edea734d1925383143a37eb535f5731ab
melicaso1624 <melicarmona35@gmail.com>
2021-08-07 21:54:11 -0500

    Eliminando archivos

--- src/components/CartWidget.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-
-export const Cart = () => {
-    return (
-        <i className="fas fa-user" style={{ fontWeight: 70 }} type="button" data-bs-toggle="modal" data-bs-target="#registerUser"></i>
-    );
-
-};
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Navbar from './components/Navbar';
-
-ReactDOM.render(<Navbar/>, document.getElementById("root"));
